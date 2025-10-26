cb04af98d842370417040d20fd8bfb65e7b7c2b1
Anthony <thesuper32203@gmail.com>
2025-10-26 19:58:32 -0400

    finished client bean config

--- src/main/java/com/chat/bank_testing_spring/config/FinicityClientBeanConfig.java
+++ src/main/java/com/chat/bank_testing_spring/config/FinicityClientBeanConfig.java
@@ -1,5 +1,23 @@
 package com.chat.bank_testing_spring.config;
 
+import com.chat.bank_testing_spring.sdk.FinicityClientFactory;
+import org.openapitools.client.ApiClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FinicityClientBeanConfig {
 
+    private final FinicityClientFactory factory;
+    private final FinicityConfig config;
+
+    public FinicityClientBeanConfig(FinicityClientFactory factory, FinicityConfig config) {
+        this.factory = factory;
+        this.config = config;
+    }
+
+    @Bean
+    public ApiClient finicityApiClient(){
+        return factory.create(config);
+    }
 }
